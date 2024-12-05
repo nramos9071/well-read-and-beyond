de81af456a5e41942b7d3420556616a5956a521c
nramos9071 <nramos9071@gmail.com>
2024-12-05 11:11:55 -0600

    working on server

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import React from 'react';
+
+const Footer = () => {
+    // const location = useLocation();
+    // const navigate = useNavigate();
+    return (
+        <footer className="footer">
+            <div>
+                <p>Created by The Group</p>
+            </div>
+        </footer>
+    );
+};
