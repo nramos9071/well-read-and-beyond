2b86a7ed91ccaad6fc679692a15d0de6ff228347
tmcdaniel94 <tmcdaniel94@gmail.com>
2024-12-05 20:29:22 -0600

    added daisyUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,15 +7,17 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Footer from './components/footer';
 
 
 function App() {
     return (
 
         <main>
+            <Header />
             <Outlet />
+            <Footer />
         </main>
     )
 }
