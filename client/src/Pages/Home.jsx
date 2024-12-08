f7e16be2c59fc03cc1545120b7e4570637ea6857
tmcdaniel94 <tmcdaniel94@gmail.com>
2024-12-07 19:59:18 -0600

    added book and movie cards to home page

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,21 +2,40 @@ import { useQuery } from '@apollo/client';
 
 import Navbar from '../components/Navbar';
 import Footer from '../components/footer';
+import movieCard from '../components/movieCard';
+import bookCard from '../components/bookCard';
 
 const Home = () => {
-    
 
     return (
         <div>
-            <Navbar />
             <div className="container">
                 <h1>Welcome to the Home Page</h1>
             </div>
+            <div className="container" >
+                <div className="myBooks" class="flex rounded box">
+                    <h3>My Books</h3>
+                    <div id="homePageBooks" class="flex-auto">
+                        <ul id="homePageBooksList" class="flex-col">
+                            {/* Append current books from user database? */}
+                            {bookCard()}
+                        </ul>
+                    </div>
+                </div>
+                <div className="movieRecs" class="flex-auto">
+                    <h3>Movie Recommendations</h3>
+                    <div id="homePageMovies" class="flex-col ">
+                        <ul id="homePageMovieList">
+                            {/* Append movie recommendations from TMDB API based on selected Book */}
+                            {/* Auto generate recommendations based on first book in list? Or favorite Genre? */}
+                            {movieCard()}
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-            <Footer />
         </div>
     )
-
 
 };
 
---
+++ client/src/components/bookCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function bookCard() {
+    return (
+        <div id="bookCard" className="card card-side bg-base-100 shadow-xl size-40">
+            <figure>
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                    alt="Books" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">Title of Book</h2>
+                {/* <p>Click the button to learn about this book.</p> */}
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">View</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default bookCard;
