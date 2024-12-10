871ff3b7a0b085cbbd7daaac141dc2a6d934d45a
nramos9071 <nramos9071@gmail.com>
2024-12-09 20:48:06 -0600

    updated resolvers for user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,14 @@ const  User  = require('../models/User');
 
 const resolvers = {
   Query: {
+    users: async () => {
+      return User.find();
+    },
+
+    user: async (parent, { userId }) => {
+        return User.findOne({ _id: userId });
+    },
+
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id });
