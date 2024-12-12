b8c0579718b2eec57f1809382a55492a2dad20ce
nramos9071 <nramos9071@gmail.com>
2024-12-12 12:24:10 -0600

    fixed login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const  User  = require('../models/User');
-
+const jwt = require('jsonwebtoken');
 const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
 
@@ -31,20 +31,25 @@ const resolvers = {
 
             return { token, user };
         },
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne ({ email });
+        login: async (parent, { username, password }) => {
+            console.log(`Attempting to log in user: ${username}`);
+            const user = await User.findOne ({ username });
            
             if (!user) {
-                throw new AuthenticationError('Incorrect credentials');
+                console.log('User not found');
+                throw new AuthenticationError('Incorrect user');
             }
 
             const correctPw = await user.isCorrectPassword(password);
+            console.log(`Password comparison result: ${correctPw}`);
 
             if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
+                console.log('Incorrect password');
+                throw new AuthenticationError('Incorrect password');
             }
 
             const token = signToken(user);
+            console.log('Login successful');
             return { token, user };
 
 
