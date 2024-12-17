3a7bdc6d6aa0b20c4b5c1133afb74d3fc76a7ee4
nramos9071 <nramos9071@gmail.com>
2024-12-16 21:06:05 -0600

    updating server

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,23 @@ dotenv.config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI)
-
-module.exports = mongoose.connection;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/well-read-beyond', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+  
+  mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to', process.env.MONGODB_URI || 'mongodb://localhost/well-read-beyond');
+  });
+  
+  mongoose.connection.on('error', (err) => {
+    console.error('Mongoose connection error:', err);
+  });
+  
+  mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+  });
+  
+  module.exports = mongoose.connection;
