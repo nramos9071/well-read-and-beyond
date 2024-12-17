9f360cc1cbe03f6045ea1104b5060fbe87874ebd
tmcdaniel94 <tmcdaniel94@gmail.com>
2024-12-16 18:07:43 -0600

    added margin to quiz box

--- client/src/Pages/Recommendations.jsx
+++ client/src/Pages/Recommendations.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Quizes from '../components/Quizes'; 
+import Quizes from '../components/Quizes';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 // Import your Quizes component
@@ -20,13 +20,13 @@ const Recommendations = () => {
       title: "What's your mood?",
       description: "Tell us your mood for recommendations?",
       image: "https://cdn.pixabay.com/photo/2019/05/04/14/22/woman-4178187_1280.jpg",
-      quizId: 2, 
+      quizId: 2,
     },
     {
       title: "What's your horror style?",
       description: "Tell us what horror you like for recommendations?",
       image: "https://cdn.pixabay.com/photo/2020/06/05/19/02/chamber-5264172_1280.jpg",
-      quizId: 3, 
+      quizId: 3,
     }
   ];
 
@@ -45,68 +45,69 @@ const Recommendations = () => {
 
   return (
     <div>
-    <div>
-    <Navbar />
-</div>
-    <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
+      <div>
+        <Navbar />
+      </div>
+      <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
 
-      <h1 className="text-2xl font-bold mb-4">
-        Not sure what book you should read next? Answer some questions and we’ll give you a book recommendation!
-      </h1>
-      <h2 className="text-xl mb-6">Click on the quiz to get started!</h2>
+        <h1 className="text-2xl font-bold mb-4">
+          Not sure what book you should read next? Answer some questions and we’ll give you a book recommendation!
+        </h1>
+        <h2 className="text-xl mb-6">Click on the quiz to get started!</h2>
 
-      {/* Carousel Container */}
-      <div className="relative w-80vw overflow-hidden">
-        {/* Cards */}
-        <div className="flex flex-nowrap transition-transform duration-500 ease-in-out">
-          {cards.map((card, index) => (
-            <div
-              key={index}
-              className={`w-full flex-shrink-0 ${index === currentIndex ? "block" : "hidden"}`}
-            >
-              <div className="card card-compact bg-base-100 shadow-xl">
-                <figure>
-                  <img src={card.image} alt="Quiz" className="w-full h-auto" />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">{card.title}</h2>
-                  <p>{card.description}</p>
-                  <div className="card-actions justify-center">
-                    <button 
-                      className="btn btn-primary"
-                      onClick={() => handleStartQuiz(card.quizId)} // Pass quizId to handleStartQuiz
-                    >
-                      Start Quiz
-                    </button>
+        {/* Carousel Container */}
+        <div className="relative w-80vw overflow-hidden">
+          {/* Cards */}
+          <div className="flex flex-nowrap transition-transform duration-500 ease-in-out">
+            {cards.map((card, index) => (
+              <div
+                key={index}
+                className={`w-full flex-shrink-0 ${index === currentIndex ? "block" : "hidden"}`}
+              >
+                <div className="card card-compact bg-base-100 shadow-xl">
+                  <figure>
+                    <img src={card.image} alt="Quiz" className="w-full h-auto" />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{card.title}</h2>
+                    <p>{card.description}</p>
+                    <div className="card-actions justify-center">
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => handleStartQuiz(card.quizId)} // Pass quizId to handleStartQuiz
+                      >
+                        Start Quiz
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+
+          {/* Carousel Controls */}
+          <button
+            className="absolute left-0 top-1/2 transform -translate-y-1/2 text-white bg-black p-2 rounded-full"
+            onClick={handlePrev}
+          >
+            Prev
+          </button>
+          <button
+            className="absolute right-0 top-1/2 transform -translate-y-1/2 text-white bg-black p-2 rounded-full"
+            onClick={handleNext}
+          >
+            Next
+          </button>
+
         </div>
 
-        {/* Carousel Controls */}
-        <button
-          className="absolute left-0 top-1/2 transform -translate-y-1/2 text-white bg-black p-2 rounded-full"
-          onClick={handlePrev}
-        >
-          Prev
-        </button>
-        <button
-          className="absolute right-0 top-1/2 transform -translate-y-1/2 text-white bg-black p-2 rounded-full"
-          onClick={handleNext}
-        >
-          Next
-        </button>
-        <div>
-          <Footer />
-        </div>
+        {/* Show Quiz Component when selected */}
+        {showQuiz && <Quizes selectedQuiz={selectedQuiz} />} {/* Pass selectedQuiz as a prop */}
+
       </div>
-
-      {/* Show Quiz Component when selected */}
-      {showQuiz && <Quizes selectedQuiz={selectedQuiz} />} {/* Pass selectedQuiz as a prop */}
-
-    </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
